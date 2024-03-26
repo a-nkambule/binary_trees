33c7fd42d6a8ec8bf36ae3d2a8ba99122c52b0d0
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-03-26 01:37:42 -0700

    binary_trees

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_node - creates a binary tree node
+ * binary_tree_node - creates a binary tree node
  * @parent: A pointer to the parent of the node to create
  * @value: The value to put in the new node
  * Return: if an error occurs - NULL

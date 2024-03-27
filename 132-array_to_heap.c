938146550c18fbf7779982d8210f74aa9ff6ec70
Aviwe Nkambule <aviwenkambule@gmail.com>
2024-03-27 06:47:24 -0700

    binary_trees

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -4,15 +4,14 @@
  * array_to_heap - builds a Max Binary Heap tree from an array
  * @array: a pointer to the first element of the array to be converted
  * @size: the number of element in the array
- *
  * Return: a pointer to the root node of the created Binary Heap
  *         NULL on failure
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
-unsigned int j;
+unsigned int i;
 heap_t *root = NULL;
-for (j = 0; j < size; j++)
-heap_insert(&root, array[j]);
+for (i = 0; i < size; i++)
+heap_insert(&root, array[i]);
 return (root)
 };
